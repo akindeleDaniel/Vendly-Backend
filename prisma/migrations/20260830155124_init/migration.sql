63d1c45f6b0380edad72b2d4f090c0c490ab190d
akindeleDaniel <ojodanielakindele@gmail.com>
2026-08-30 16:57:03 +0100

    feat: add Prisma, define Listing model, run initial migration

---
+++ prisma/migrations/20260830155124_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Listing" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Listing_pkey" PRIMARY KEY ("id")
+);
