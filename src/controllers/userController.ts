80e4d571faf32873c2a943863eacc5994f9dd5a1
akindeleDaniel <ojodanielakindele@gmail.com>
2026-09-07 16:59:15 +0100

    feat: add login endpoint using jwt

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt"
 import prisma from "../lib/prisma.js"
 import type { Request, Response } from "express"
+import jwt from "jsonwebtoken"
 
 export const createUser = async(req: Request, res: Response) => {
     const {name, email, password} = req.body
@@ -29,4 +30,36 @@ export const createUser = async(req: Request, res: Response) => {
     }catch(error){
         res.status(500).send("Sorry there is an issue on our end")
     }
+}
+
+
+export const loginUser = async(req: Request, res: Response) =>{
+    const {email, password} = req.body
+
+    if(!email || !password){
+        res.status(400).send("All areas are required")
+        return
+    }
+
+    const existingUser = await prisma.user.findUnique({where:{email}})
+
+    if (!existingUser){
+        res.status(400).send("Invalid email or password")
+        return
+    }
+
+    const passwordMatches = await bcrypt.compare(password, existingUser.passwordHash)
+
+    if(!passwordMatches){
+        res.status(400).send("Invalid email or password")
+        return
+    }
+    
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not set");
+    }
+    const token = jwt.sign({id: existingUser.id}, process.env.JWT_SECRET, {expiresIn:"1h"})
+
+    
+    res.send({message: "Login Successful", token})
 }
